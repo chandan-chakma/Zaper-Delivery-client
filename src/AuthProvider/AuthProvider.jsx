982092cbc273ad0464e5f568dd5a1c24a0688608
chandan-chakma <cchakma19@gmail.com>
2026-08-28 04:16:58 +0800

    contex-auth

---
+++ src/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,26 @@
+import React, { Children, createContext, useContext } from 'react';
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../Firebase/Firebase.config.js';
+const AuthContext = createContext()
+const AuthProvider = ({children}) => {
+
+    const createEmailUser = (email,password) => {
+       return createUserWithEmailAndPassword(auth,email,password)
+    }
+    const signInEmailUser = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const authInfo = {
+        createEmailUser,
+        signInEmailUser
+        
+    }
+    return (
+        <AuthContext value={authInfo}>
+            {children}
+        </AuthContext>
+    );
+};
+
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import { RouterProvider } from "react-router/dom";
 import { router } from './Routes/Router.jsx'
+import AuthProvider from './AuthProvider/AuthProvider.jsx';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}>
-    </RouterProvider>
+    <AuthProvider>
+      <RouterProvider router={router}>
+      </RouterProvider>
+    </AuthProvider>
+   
   </StrictMode>,
 )
